b93e779694eb3e3f2b195acaed3b58ed4b43fabb
Sergey Lugovoy <sergey.lugovoy@resmed.com>
2019-02-18 12:01:08 +0200

    Initial commit

---
+++ src/main/java/com/example/pcfdemo1/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.pcfdemo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping(value = "/greet/{name}")
+    public String elements(@PathVariable String name) {
+        return "Hello " + name + " from Cloud Foundry!";
+    }
+
+}
---
+++ src/test/java/com/example/pcfdemo1/PcfDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.pcfdemo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PcfDemo1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
